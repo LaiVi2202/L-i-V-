b45177d935b06f04beba092bc6b35b3008cce8d1
LaiVi2202 <95302980+LaiVi2202@users.noreply.github.com>
2021-12-02 18:15:04 +0700

    de

---
+++ Demo1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main (){
+
+    // input A,B
+	int A,B;
+	printf("Nhap A=");
+	scanf("Xd" ,&A);
+	printf("Nhap B=");
+	scanf("Xd" ,&B);
+	
+	// so sanh A > B
+     if(A>B){
+     	// Yes
+     	printf("So lon hon: %d" ,A);
+     }else{
+     	//No
+     	printf("So lon hon: %d" ,B);
+    
+	 }
+	 }
+	 
